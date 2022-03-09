108178595a5ff39067cabb906d2cc9dbf787e506
Ahsan <achsan.ajr@gmail.com>
2022-03-09 16:25:55 +0700

    coba category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -6,7 +6,7 @@ exports.getCategories = async (req, res) => {
       attributes: {
         exclude: ["createdAt", "updatedAt"],
       },
-      order: [["name", "ASC"]],
+      order: [["id", "ASC"]],
     });
 
     res.status(200).send({
